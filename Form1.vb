08493b787be323065dfbb54c847d0977fcfd00dd
Dell-User <Dell-User@DESKTOP-S7HKJ5C>
2024-01-18 08:15:26 +0200

    Add project files.

---
+++ Form1.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim x, y As Double
+        Dim func, fun, result As Double
+        x = Val(TextBox1.Text)
+        y = Val(TextBox2.Text)
+
+        func = Math.Cos(x)
+        fun = Math.Sin(y)
+        result = func * fun
+
+        MsgBox("result is " & result)
+    End Sub
+End Class
